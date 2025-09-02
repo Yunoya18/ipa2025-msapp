97c5945ca1bb2ebb9a3a12b5aa522713f8d86ac9
yunoya18 <sonrasamon15725@gmail.com>
2025-09-02 15:28:52 +0700

    show router details

--- web/app.py
+++ web/app.py
@@ -11,11 +11,12 @@ db_name    = os.environ.get("DB_NAME")
 
 client = MongoClient(mongo_uri)
 mydb = client[db_name]
-mycol = mydb["routers"]
+routers = mydb["routers"]
+interface_status = mydb["interface_status"]
 
 @app.route("/")
 def main():
-    return render_template("index.html", data=mycol.find({}))
+    return render_template("index.html", data=routers.find({}))
 
 @app.route("/add", methods=["POST"])
 def add_ip():
@@ -24,7 +25,7 @@ def add_ip():
     password = request.form.get("password")
 
     if ip and username and password:
-        mycol.insert_one({'ip': ip, 'username': username, 'password': password})
+        routers.insert_one({'ip': ip, 'username': username, 'password': password})
     return redirect("/")
 
 @app.route("/delete", methods=["POST"])
@@ -32,10 +33,15 @@ def delete_ip():
     try:
         idx = request.form.get("idx")
         myquery = {'_id': ObjectId(idx)}
-        mycol.delete_one(myquery)
+        routers.delete_one(myquery)
     except Exception:
         pass
     return redirect(url_for("main"))
+
+@app.route("/router/<ip>", methods=["GET"])
+def get_router(ip):
+    router_details = interface_status.find({"router_ip": ip}).sort("timestamp", -1).limit(3)
+    return render_template("router_details.html", router_ip=ip, router_details=router_details)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
